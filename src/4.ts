96e13ed70fcfa1a5a1fe7832016f4876241060cb
oleg-usatyi <oleg_usatyi@ukr.net>
2024-08-11 14:57:21 +0200

    OOP hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {
+  }
+
+  comeIn(person: Person) {
+    if(this.door) {
+      this.tenants.push(person);
+      console.log("Person entered the house.");
+    } else {
+      console.log("The door is closed. The person cannot enter.");
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if(key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is now open.");
+    } else {
+      console.log("The key does not fit. The door remains closed.");
+    }
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
